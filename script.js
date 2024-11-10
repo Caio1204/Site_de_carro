e55c1d506d1ca22bbc1e77157827fddda7f4537b
Caio1204 <162984113+Caio1204@users.noreply.github.com>
2024-11-10 13:56:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function adicionarCarro() {
     const cor = document.getElementById('cor').value;
     const preco = document.getElementById('preco').value;
     const contato = document.getElementById('contato').value;
+    const comentario = document.getElementById('comentario').value;
     const foto = document.getElementById('foto').files[0];
 
     const reader = new FileReader();
@@ -20,6 +21,7 @@ function adicionarCarro() {
             cor,
             preco,
             contato,
+            comentario,
             foto: e.target.result
         };
 
@@ -42,13 +44,13 @@ function atualizarCatalogo() {
             
             <img src="${carro.foto}" alt="${carro.marcaModelo}">
             <h3>${carro.Modelo}</h3>
-
             <h3>${carro.Marca}</h3>
             <p style="padding:3%">Ano: ${carro.ano}</p>
             <p style="padding:3%">Quilometragem: ${carro.quilometragem} km</p>
             <p>Cor: ${carro.cor}</p>
             <p>Preço: ${carro.preco}</p>
             <p>Contato: ${carro.contato}</p>
+            <p>Comentario: ${carro.comentario}</p>
             <button onclick="verDetalhes(${index})">Ver Detalhes</button>
             <button class="excluir-btn" onclick="excluirCarro(${index})">Excluir</button>
         `;
@@ -75,13 +77,14 @@ window.onload = function() {
         const carro = JSON.parse(localStorage.getItem('detalhesCarro'));
         detalhesCarro.innerHTML = `
             <div class="detalhes-foto detalhes-dados">
-            <img src="${carro.foto}" alt="${carro.marcaModelo}" class="image" >
+            <img src="${carro.foto}" alt="${carro.marcaModelo}" class="image" max >
             <h2>${carro.Marca} ${carro.Modelo} </h2>
             <p>Ano: ${carro.ano}</p></br>
             <p>Quilometragem: ${carro.quilometragem} Km </p>
             <p>Cor: ${carro.cor}</p>
-            <p>Preço: ${carro.preco}</p>
+            <p>Preço:$${carro.preco}</p>
             <p>Contato: ${carro.contato}</p>
+            <br><p> Comentario:${carro.comentario}</p></br> 
            </div>
         `;
     } else {
